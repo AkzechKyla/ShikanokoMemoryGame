573eae9fc7a1cf54d5c658c529586d5493650180
Kyla <kflmorcillos@gmail.com>
2024-09-13 22:27:28 +0800

    Change id value to string

--- scripts/card.js
+++ scripts/card.js
@@ -3,55 +3,64 @@ export class Card {
         this.id = cardDetails.id;
         this.value = cardDetails.imageURL;
         this.cardBack = cardDetails.cardBack;
+        this.isFlipped = false;
+    }
+
+    getCard(id) {
+        if (this.id === id) {
+            return this.card;
+        }
+
+        return null;
     }
 }
 
 export const cards = [{
-    id: 1,
+    id: '1',
     imageURL: 'CARD 1',
     cardBack: 'CARDBACK 1',
 }, {
-    id: 2,
+    id: '2',
     imageURL: 'CARD 2',
     cardBack: 'CARDBACK 2',
 }, {
-    id: 3,
+    id: '3',
     imageURL: 'CARD 3',
     cardBack: 'CARDBACK 3',
 }, {
-    id: 4,
+    id: '4',
     imageURL: 'CARD 4',
     cardBack: 'CARDBACK 4',
 }, {
-    id: 5,
+    id: '5',
     imageURL: 'CARD 5',
     cardBack: 'CARDBACK 5',
 }, {
-    id: 6,
+    id: '6',
     imageURL: 'CARD 6',
     cardBack: 'CARDBACK 6',
 }, {
-    id: 7,
+    id: '7',
     imageURL: 'CARD 1',
     cardBack: 'CARDBACK 1',
 }, {
-    id: 8,
+    id: '8',
     imageURL: 'CARD 2',
     cardBack: 'CARDBACK 2',
 }, {
-    id: 9,
+    id: '9',
     imageURL: 'CARD 3',
     cardBack: 'CARDBACK 3',
 }, {
-    id: 10,
+    id: '10',
     imageURL: 'CARD 4',
     cardBack: 'CARDBACK 4',
 }, {
-    id: 11,
+    id: '11',
     imageURL: 'CARD 5',
     cardBack: 'CARDBACK 5',
 }, {
-    id: 12,
+    id: '12',
     imageURL: 'CARD 6',
     cardBack: 'CARDBACK 6',
 }].map((cardDetails) => {
