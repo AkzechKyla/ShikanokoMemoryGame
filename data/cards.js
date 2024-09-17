6742334644e3b96ecad532d85e1fdab08dff91d3
Kyla <kflmorcillos@gmail.com>
2024-09-17 22:24:44 +0800

    Move card image urls to data directory

---
+++ data/cards.js
@@ -0,0 +1,8 @@
+export const cardImages = [
+    'https://i.pinimg.com/736x/19/59/83/1959833460da93ea63ee9404b159624d.jpg',
+    'https://i.pinimg.com/736x/4e/ed/a3/4eeda30f3c116270f1505d8cb107b329.jpg',
+    'https://i.pinimg.com/736x/b6/84/11/b684114961804efe7790af4315a8f742.jpg',
+    'https://i.pinimg.com/736x/29/11/32/29113238141dfedeacd46c2fdf989989.jpg',
+    'https://i.pinimg.com/736x/4e/1a/8f/4e1a8f51e93ddfe3a16020d8551bc485.jpg',
+    'https://i.pinimg.com/736x/05/49/a4/0549a427a95edcb90a33d52b8a0ffb6f.jpg'
+];
