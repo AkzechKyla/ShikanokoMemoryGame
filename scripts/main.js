6c1fd8d945a9f437a6ca3442b248e24972cd3db1
Kyla <kflmorcillos@gmail.com>
2024-09-17 22:26:15 +0800

    Use flatMap and map to duplicate card image urls

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,13 @@
 import {ScenesManager} from './scenesManager.js';
 import {GameBoard} from './gameBoard.js';
-import {Card, cards} from './card.js';
+import {Card} from './card.js';
+import {cardImages} from '../data/cards.js';
 
 function startGame() {
     const gameScenes = new ScenesManager(['main-menu', 'game']);
     gameScenes.switchScene('game');
+
+    const cards = cardImages.flatMap((card) => [card, card]).map((card) => new Card(card));
 
     const gameBoard = new GameBoard(cards);
     gameBoard.render();
