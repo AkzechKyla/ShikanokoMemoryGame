8fcb71fc80a221730ccdde33d386b6945d9041c0
Kyla <kflmorcillos@gmail.com>
2024-09-27 19:19:05 +0800

    AudioPlayer -> Audio

--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,14 @@ import {GameBoard} from './gameBoard.js';
 import {Card} from './card.js';
 import {cardImages} from '../data/cards.js';
 import {Modal} from './modalManager.js';
-import {AudioPlayer} from './audioPlayer.js';
+import {Audio} from './audio.js';
 
 async function main() {
     const gameScenes = new ScenesManager(['main-menu', 'game']);
     const endGameModal = new Modal(['end-game']);
     const cards = cardImages.flatMap((card) => [card, card]).map((card) => new Card(card));
-    const audioPlayer = new AudioPlayer();
-    const gameBoard = new GameBoard(gameScenes, endGameModal, cards, audioPlayer);
+    const backgroundMusic = new Audio('background-music');
+    const gameBoard = new GameBoard(gameScenes, endGameModal, cards, backgroundMusic);
 
     window.gameScenes = gameScenes;
     window.endGameModal = endGameModal;
