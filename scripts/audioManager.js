c928d86f2e677c4e2d66d9796e26c4f037007b6e
Kyla <kflmorcillos@gmail.com>
2024-10-06 13:29:00 +0800

    Add toggleSound and toggleMusic

--- scripts/audioManager.js
+++ scripts/audioManager.js
@@ -6,4 +6,28 @@ export class AudioManager {
         this.sound = sound;
         this.music = music;
     }
+
+    toggleSound() {
+        this.sound.isAudioOn = !this.sound.isAudioOn;
+
+        if (this.sound.isAudioOn) {
+            this.soundButton.innerHTML = 'Sound On';
+        } else {
+            this.soundButton.innerHTML = 'Sound Off';
+        }
+
+        console.log(this.sound.isAudioOn);
+    }
+
+    toggleMusic() {
+        this.music.isAudioOn = !this.music.isAudioOn;
+
+        if (this.music.isAudioOn) {
+            this.musicButton.innerHTML = 'Music On';
+        } else {
+            this.musicButton.innerHTML = 'Music Off';
+        }
+
+        console.log(this.music.isAudioOn);
+    }
 }
